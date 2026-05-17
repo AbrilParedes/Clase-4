f5d83e4c94c4024eb6264aef3ebd2349ce5df469
AbrilParedes <luisa.azami@gmail.com>
2026-05-17 12:46:55 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// variables
+let mensaje = "Hola, este es un mensaje normal";
+let numero = 48;
+let estado = true;
+//constante s
+const PI = 3.14159;
+console.log(mensaje);
+console.warn(numero);
+console.error("Esto es un error!");
+console.log("Valor de PI: " + PI);
+console.log(typeof estado) //boolean
+console.log(typeof numero) //number
+console.log(typeof mensaje) //string
